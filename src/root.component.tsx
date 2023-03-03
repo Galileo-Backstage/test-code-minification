0a3c768bfce43d898f802f2ab34aa5924a824c17
Volodymyr Pankiv <Volodymyr.Pankiv@teck.com>
2023-03-03 14:37:24 +0200

    hardcode vars

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -12,17 +12,22 @@ export default function Root() {
 
 const MainPage = () => {
   const logger = GalileoFrontendLogger.createLogger({
-    connectionString: process.env.REACT_APP_CONNECTION_STRING,
+    connectionString: 'InstrumentationKey=ce891b81-4313-4619-8156-75ba38e07d42;IngestionEndpoint=https://centralus-0.in.applicationinsights.azure.com/;LiveEndpoint=https://centralus.livediagnostics.monitor.azure.com/',
   });
 
   const trackPageView = () => {
     logger.trackPageView({ name: "React microfrontend app" });
   };
 
+  const throwError = () => {
+    throw new Error('Test error from deployed app');
+  }
+
   return (
     <div className="main-container">
       <h1>Galileo logging library (React microfrontend)</h1>
       <button onClick={() => trackPageView()}>Track page view</button>
+      <button onClick={() => throwError()}>Throw error</button>
     </div>
   );
 };
